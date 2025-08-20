b02b8cd6bcfd2246b7fdc43fe84e70e133c05273
Dilnoza Mullakhanova <82514035+dileen-mollis@users.noreply.github.com>
2025-08-20 22:06:56 +0500

    Create dad_is_Commatose.py

---
+++ Codewars/Solutions/Python/dad_is_Commatose.py
@@ -0,0 +1,9 @@
+""" Task
+
+Your dad doesn't really get punctuation, and he's always putting extra commas in when he posts. You can tolerate the run-on sentences, as he does actually talk like that, but those extra commas have to go!
+
+Write a function that takes a string as an argument and returns a string with the extraneous commas removed. The returned string should not end with a comma or have any trailing whitespace.
+"""
+
+def dad_filter(string):
+    return ",".join(s for s in string.split(",") if s!="").rstrip(", ")
